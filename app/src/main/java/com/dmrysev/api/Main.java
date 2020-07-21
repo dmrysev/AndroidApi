b2a0ea3c58368d04b52e6ec0b7be49b7723114b5
dmrysev <dmrysev@gmail.com>
2020-07-21 14:32:25 +0300

    Added sources

---
+++ app/src/main/java/com/dmrysev/api/Main.java
@@ -0,0 +1,69 @@
+package com.dmrysev.api;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int action = getIntent().getExtras().getInt("action");
+        switch (action) {
+            case 0:
+                mute();
+                break;
+            case 1:
+                unmute();
+                break;
+        }
+        finish();
+    }
+    private void requestMutePermissions() {
+        try {
+            if (Build.VERSION.SDK_INT < 23) {
+                AudioManager audioManager = (AudioManager)getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+                audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+            } else if( Build.VERSION.SDK_INT >= 23 ) {
+                this.requestForDoNotDisturbPermissionOrSetDoNotDisturbForApi23AndUp();
+            }
+        } catch ( SecurityException e ) {
+
+        }
+    }
+
+    private void requestForDoNotDisturbPermissionOrSetDoNotDisturbForApi23AndUp() {
+
+        NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        // if user granted access else ask for permission
+        if ( notificationManager.isNotificationPolicyAccessGranted()) {
+            AudioManager audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+            audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+        } else{
+            // Open Setting screen to ask for permisssion
+            Intent intent = new Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+            startActivityForResult( intent, 0 );
+        }
+    }
+
+    public void mute() {
+        requestForDoNotDisturbPermissionOrSetDoNotDisturbForApi23AndUp();
+    }
+
+    public void mutePnone(View view) {
+        requestForDoNotDisturbPermissionOrSetDoNotDisturbForApi23AndUp();
+    }
+
+    public void unmute() {
+        AudioManager audioManager = (AudioManager)getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+        audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+    }
+
+}
